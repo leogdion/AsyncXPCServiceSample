5cb77686da5d231c53beaa34770c54efbbe68a9d
leogdion <leogdion@brightdigit.com>
2025-02-20 10:58:24 -0500

    working with async stuff

--- AsyncXPCServiceSample/Sources/AsyncXPC/AsyncXPCServiceSampleXPC.swift
+++ AsyncXPCServiceSample/Sources/AsyncXPC/AsyncXPCServiceSampleXPC.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 import AsyncServiceXPC
+import AsyncService
 
 /// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
 class AsyncXPCServiceSampleXPC: NSObject, AsyncXPCServiceSampleXPCProtocol {
-    
+  let implementation = AsyncServiceImpl()
     /// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
-    @objc func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void) {
-        let response = firstNumber + secondNumber
-        reply(response)
+  @objc  func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @Sendable @escaping (Int) -> Void) {
+    Task { [implementation] in
+
+        
+        let value = await implementation.performCalculation(firstNumber: firstNumber, secondNumber: secondNumber)
+
+        reply(value)
+
     }
+  }
 }
