bbcb9bcba0aca0d17df6c8b87b89acd0e1626dca
leogdion <leogdion@brightdigit.com>
2025-02-20 09:48:22 -0500

    working sample

---
+++ AsyncXPCServiceSample/Sources/AsyncXPC/AsyncXPCServiceSampleXPC.swift
@@ -0,0 +1,19 @@
+//
+//  AsyncXPCServiceSampleXPC.swift
+//  AsyncXPCServiceSampleXPC
+//
+//  Created by Leo Dion on 2/20/25.
+//
+
+import Foundation
+import AsyncService
+
+/// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
+class AsyncXPCServiceSampleXPC: NSObject, AsyncXPCServiceSampleXPCProtocol {
+    
+    /// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+    @objc func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void) {
+        let response = firstNumber + secondNumber
+        reply(response)
+    }
+}
---
+++ AsyncXPCServiceSample/Sources/AsyncXPC/ServiceDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  AsyncXPCServiceSampleXPC
+//
+//  Created by Leo Dion on 2/20/25.
+//
+
+import Foundation
+import AsyncService
+
+final public class ServiceDelegate: NSObject, NSXPCListenerDelegate {
+    
+    /// This method is where the NSXPCListener configures, accepts, and resumes a new incoming NSXPCConnection.
+  public func listener(_ listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
+        
+        // Configure the connection.
+        // First, set the interface that the exported object implements.
+        newConnection.exportedInterface = NSXPCInterface(with: AsyncXPCServiceSampleXPCProtocol.self)
+        
+        // Next, set the object that the connection exports. All messages sent on the connection to this service will be sent to the exported object to handle. The connection retains the exported object.
+        let exportedObject = AsyncXPCServiceSampleXPC()
+        newConnection.exportedObject = exportedObject
+        
+        // Resuming the connection allows the system to deliver more incoming messages.
+        newConnection.resume()
+        
+        // Returning true from this method tells the system that you have accepted this connection. If you want to reject the connection for some reason, call invalidate() on the connection and return false.
+        return true
+    }
+}
